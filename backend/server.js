fb58f51dd9df947916f3f8f2e8ba502ae96671dc
saurabh <rathvasaurabhkumar@gmail.com>
2024-06-10 02:40:58 +0530

    backend completed for the chat app

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+const PORT = process.env.PORT || 5000;
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.post("/", (req, res) => {
+  console.log(req.body, "testing the code ");
+  res.send("hello");
+});
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes); 
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
+});
