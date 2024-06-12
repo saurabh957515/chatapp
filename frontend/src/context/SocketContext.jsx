671180b44dd96cc9d3df016854f85484a66b8e63
Vardaam <team@vardaam.com>
2024-06-12 17:01:11 +0530

    process env updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -12,10 +12,10 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
   const [onlineUsers, setOnlineUsers] = useState([]);
   const { authUser } = useAuthContext();
-//"https://live-chat-weyr.onrender.com"
+  //"https://live-chat-weyr.onrender.com"
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io(process.env.REACT_APP_SOCKET_URL, {
         query: {
           userId: authUser._id,
         },
