e9dc440df35b7c5ba43b84cbe25c4e36aa1dadf0
saurabh <rathvasaurabhkumar@gmail.com>
2024-08-20 08:22:18 +0530

    test change

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -5,7 +5,7 @@ export const AuthContext = createContext();
 // eslint-disable-next-line react-refresh/only-export-components
 export const useAuthContext = () => {
 	return useContext(AuthContext);
-};
+}
 
 export const AuthContextProvider = ({ children }) => {
 	const [authUser, setAuthUser] = useState(JSON.parse(localStorage.getItem("chat-user")) || null);
